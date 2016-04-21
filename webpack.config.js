8e9d0076cb947a48294736f29beb8cae21e14f34
Adam <ebokianknight@gmail.com>
2016-04-21 08:19:20 -0400

    Mario on Ice

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  context: __dirname,
+  entry: "./lib/root.js",
+  output: {
+    path: "./lib/",
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ["", ".js", ".jsx"]
+  }
+};
